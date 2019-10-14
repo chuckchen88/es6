885b51d7b3296c07353c07e0d95e45f65f4536dd
chuckchen <chenl@m.apkbus.com>
2019-10-14 15:08:27 +0800

    commit

--- app/js/lottery/calculate.js
+++ app/js/lottery/calculate.js
@@ -10,6 +10,47 @@ class calculate{
 		return count;
 	}
 
+	//奖金范围
+	computeBouns(active,play_name){
+		const play = play_name.split('');
+		const self = this;
+		let arr = new Array(active).fill(0);
+		let min,max;
+		if(play[0]==='r'){
+			let min_active = 5-(11-active);
+			if(min_active>0){
+				if(min_active-play[1]>=0){
+					arr = new Array(min_active).fill(0);
+					min = Calculate.combine(arr,play[1]).length;
+				}else{
+					if(play[1]-5>0&&active-play[1]>=0){
+						arr = new Array(active-5).fill(0);
+						min = Calculate.combine(arr,play[1]-5).length;
+					}else{
+						min = active-play[1]>-1?1:0;
+					}
+				}
+			}else{
+				min = active-play[1]>-1?1:0;
+			}
+			let max_active = Math.min(active,5);
+			if(play[1]-5>0){
+				if(active-play[1]>=0){
+					arr = new Array(active-5).fill(0);
+					max = Calculate.combine(arr,play[1]-5).length;
+				}else{
+					max = 0;
+				}
+			}else if(play[1]-5<0){
+				arr = new Array(Math.min(active,5)).fill(0);
+				max = Calculate.combine(arr,play[1]).length;
+			}else{
+				max = 1;
+			}
+		}
+		return [min,max].map(item=>item*self.play_list.get(play_name).bonus);
+	}
+
 	//组合运算
 	static combine(arr,size){
 		let allResult = [];
@@ -36,3 +77,4 @@ class calculate{
 		})(arr,size,[])
 	}
 }
+export default Calculate;
